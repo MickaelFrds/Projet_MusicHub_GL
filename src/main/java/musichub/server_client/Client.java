546465959f1a827cb56fb077eba68959345e3343
Mickael Fernandes <mickael.fernandes.0@gmail.com>
2021-04-06 13:15:29 +0200

    Initial commit

--- src/main/java/musichub/server_client/Client.java
+++ src/main/java/musichub/server_client/Client.java
@@ -3,27 +3,20 @@ package musichub.server_client;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
 
     private Socket socket;
+    ObjectOutputStream out;
+    ObjectInputStream in;
 
-    public void connect(String ip)
-    {
+    public void connect(String ip) {
         int port = 6000;
-        try  {
+        try {
             socket = new Socket(ip, port);
-
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            out.writeObject("hello");
-            out.flush();
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            System.out.println("Client a cree les flux");
-            Object recu = in.readObject();
-            System.out.println("Serveur : " + recu);
-            recu = in.readObject();
-            System.out.println(recu);
-        } catch  (IOException | ClassNotFoundException uhe) {
+            requestClient(socket);
+        } catch (IOException | ClassNotFoundException uhe) {
             uhe.printStackTrace();
         } finally {
             try {
@@ -34,18 +27,38 @@ public class Client {
         }
     }
 
-    public void addSong(){
+
+    public void requestClient(Socket socket) throws IOException, ClassNotFoundException {
+
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+        Scanner scan = new Scanner(System.in);
+        Object cmd ;
+        while (true){
+            cmd = scan.nextLine();
+            if (cmd.equals("h"))printAvailableCommands();
+            out.writeObject(cmd);
+            Object recu = in.readObject();
+            System.out.println(recu);
+        }
     }
 
-    public void addAlbum(){
+    private void printAvailableCommands() {
+        System.out.println("a: display the album titles");
+        System.out.println("p: display the playlist titles");
+        System.out.println("s: display all songs");
 
+        System.out.println("+a: add a new song");
+        System.out.println("+p: add a new album");
+        System.out.println("+s: add a new playlist");
+        System.out.println("h: menu");
+        /*
+        System.out.println("+: add a song to an album");
+        System.out.println("p: create a new playlist from existing songs and audio books");
+        System.out.println("-: delete an existing playlist");
+        System.out.println("s: save elements, albums, playlists");
+        System.out.println("q: quit program");
+         */
     }
 
-    public void addPlaylist(){
-
-    }
-
-    public void getAlbum()  {
-
-    }
 }
--- src/main/java/musichub/server_client/Server.java
+++ src/main/java/musichub/server_client/Server.java
@@ -1,26 +1,24 @@
 package musichub.server_client;
 
+import musichub.Exception.NoElementFoundException;
+import musichub.Exception.NoPlayListFoundException;
+
 import java.io.*;
 import java.net.*;
 
 public class Server {
 
+
+
     public void run (String ip) {
         try {
            ServerSocket ss = new ServerSocket (6000 );
-           System.out.println("En attente de connexion...");
+          //System.out.println("En attente de connexion...");
            Socket socket = ss.accept();
-           System.out.println("client connecté");
-
-           //request
-           ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-           out.writeObject("Bienvenue");
-           ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-           System.out.println("Le serveur a créé les flux");
-
-           Object recu = in.readObject();
-           System.out.println("Client :"+ recu);
-        } catch (IOException | ClassNotFoundException e) {e.printStackTrace();}
+          //System.out.println("client connecté");
+           RequestManager requestManager1 = new RequestManager(socket);
+           requestManager1.request();
+        } catch (IOException | ClassNotFoundException | NoPlayListFoundException | NoElementFoundException e) {e.printStackTrace();}
     }
 
 
