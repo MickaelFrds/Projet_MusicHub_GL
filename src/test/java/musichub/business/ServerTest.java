370b33a134d937fe9b32ff34cd95ac4d881ff96f
Mickael Fernandes <mickael.fernandes.0@gmail.com>
2021-04-06 13:10:49 +0200

    Initial commit

--- src/test/java/musichub/business/ServerTest.java
+++
@@ -1,9 +0,0 @@
-package musichub.business;
-
-import java.net.Socket;
-import static org.junit.jupiter.api.Assertions.*;
-
-class ServerTest {
-    Server s1 = new Server();
-    Socket clientSocket = new Socket(8254);
-}
