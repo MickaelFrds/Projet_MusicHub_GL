c028822960dd834c1bf7a864686df4351680f75c
MickaelFrds <mickael.fernandes.0@gmail.com>
2021-04-11 16:14:35 +0200

    Merge pull request #7 from MickaelFrds/Ahmed
    v2

---
+++ src/main/java/musichub/business/AudioPlayer.java
@@ -0,0 +1,54 @@
+package musichub.business;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class AudioPlayer {
+
+    private File file;
+    private AudioInputStream audioInput;
+    private AudioFormat audioFormat;
+    private int bytesPerFrame;
+    private int numBytes;
+    private byte[] tab;
+    private DataLine.Info info;
+    public SourceDataLine line;
+
+    public AudioPlayer(String titleSong){
+        try {
+            String chemin = System.getProperty("user.dir") + "\\files\\" + titleSong + ".wav";
+            this.file= new File(chemin);
+            this.audioInput = AudioSystem.getAudioInputStream(file);
+            this.bytesPerFrame = audioInput.getFormat().getFrameSize();
+            this.numBytes = 1024*bytesPerFrame;
+            this.tab =new byte[numBytes];
+            this.audioFormat = audioInput.getFormat();
+            this.info = new DataLine.Info(SourceDataLine.class,audioFormat);
+            this.line = (SourceDataLine) AudioSystem.getLine((info));
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public void run(){
+        try {
+            line.open(audioFormat);
+            line.start();
+            int nb;
+            while ((nb = audioInput.read(tab,0,numBytes )) != -1 ) {
+                line.write(tab, 0, nb);
+            }
+            line.close();
+        } catch (LineUnavailableException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
+
