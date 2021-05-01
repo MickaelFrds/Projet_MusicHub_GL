f9be9e4d81fe76b49b57bbda74fa364321cd7f07
MickaelFrds <mickael.fernandes.0@gmail.com>
2021-05-01 22:47:55 +0200

    Merge remote-tracking branch 'origin/Mickael' into Mickael

--- src/main/java/musichub/server_client/RequestManager.java
+++ src/main/java/musichub/server_client/RequestManager.java
@@ -16,6 +16,10 @@ import java.util.List;
 
 
 public class RequestManager {
+    /**
+     *
+     */
+
 
     private final ObjectInputStream in;
     private final ObjectOutputStream out;
@@ -90,16 +94,45 @@ public class RequestManager {
     }
 
     public void createSong() throws IOException, ClassNotFoundException {
+        Object sTitle,sArtist,sLength,sContent, sGenre;
         out.writeObject("Title :");
-        Object sTitle = in.readObject();
+        do{
+            sTitle = in.readObject();
+            if(sTitle==""){out.writeObject("The title should not be empty");}
+        }while (sTitle=="");
         out.writeObject("Artiste :");
-        Object sArtist = in.readObject();
+        do{
+            sArtist = in.readObject();
+            if(sArtist==""){out.writeObject("The artist should not be empty");}
+        }while (sArtist=="");
         out.writeObject("Length :");
-        Object sLength = in.readObject();
+        Integer number;
+        do{
+            sLength = in.readObject();
+            try{
+                number=(Integer.parseInt((String)sLength));
+            }catch (NumberFormatException e){ System.err.println("un nombre entier est attendu");}
+            if(sLength=="" || (Integer.parseInt((String)sLength)<1)){out.writeObject("The Length format is not valid");}
+        }while (sLength=="" || (Integer.parseInt((String)sLength)<1));
         out.writeObject("Content :");
-        Object sContent = in.readObject();
+        do{
+            sContent = in.readObject();
+            if(sContent==""){out.writeObject("The content should not be empty");}
+        }while (sContent=="");
         out.writeObject("Genre : (jazz,classic,hiphop,rock,pop,rap)");
-        Object sGenre = in.readObject();
+        String type;
+        List<String> type2 = new LinkedList<>();
+        type2.add("jazz");
+        type2.add("classic");
+        type2.add("pop");
+        type2.add("hiphop");
+        type2.add("rock");
+        type2.add("rap");
+        do{
+            sGenre = in.readObject();
+            type=(String) sGenre;
+            if(!type2.contains(type)){out.writeObject("The genre doesn't match, try again");}
+        }while (!type2.contains(type));
         Song s =new Song( (String) sTitle, (String) sArtist, Integer.parseInt((String) sLength) , (String) sContent, (String) sGenre);
         out.writeObject("Song :" + s.getTitle()+ "has been created !\n");
         musicHub.addElement(s);
@@ -108,13 +141,32 @@ public class RequestManager {
 
     public void createAlbum() throws IOException, ClassNotFoundException {
         out.writeObject("Title :");
-        Object aTitle = in.readObject();
+        Object aTitle,aArtist,aLength,aDate;
+        do{
+            aTitle = in.readObject();
+            if(aTitle==""){out.writeObject("The title should not be empty");}
+        }while (aTitle=="");
         out.writeObject("Artiste :");
-        Object aArtist = in.readObject();
+        do{
+            aArtist = in.readObject();
+            if(aArtist==""){out.writeObject("The artist should not be empty");}
+        }while (aArtist=="");
         out.writeObject("Lenght :");
-        Object aLength = in.readObject();
+        Integer number;
+        do{
+            aLength = in.readObject();
+            try{
+                number=(Integer.parseInt((String)aLength));
+            }catch (NumberFormatException e){ out.writeObject("un nombre entier est attendu");}
+            if(aLength=="" || (Integer.parseInt((String)aLength)<1)){out.writeObject("The Length format is not valid");}
+        }while (aLength=="" || (Integer.parseInt((String)aLength)<1));
         out.writeObject("Date : (format : YYYY-DD-MM)");
-        Object aDate = in.readObject();
+        String date;
+        do{
+            aDate = in.readObject();
+            date=(String)aDate;
+            if(date.length()<10 || date.length()>10 || date.charAt(4)!='-' || date.charAt(7)!='-'){out.writeObject("The date format is not valid, try again");}
+        }while (date.length()<10 || date.charAt(4)!='-' || date.charAt(7)!='-');
         Album a = new Album((String) aTitle,(String) aArtist,Integer.parseInt((String)aLength),(String)aDate);
         out.writeObject("Album : "+a.getTitle()+" has been created !\n");
         musicHub.addAlbum(a);
@@ -124,7 +176,11 @@ public class RequestManager {
 
     public void createPlaylist() throws IOException, ClassNotFoundException {
         out.writeObject("Type the name of the playlist you wish to create:");
-        Object titlePlaylist = in.readObject();
+        Object titlePlaylist;
+        do{
+            titlePlaylist = in.readObject();
+            if(titlePlaylist==""){out.writeObject("The name should not be empty");}
+        }while (titlePlaylist=="");
         PlayList pl = new PlayList((String) titlePlaylist);
         out.writeObject("Playlist " + pl.getTitle() + " is created\n");
         musicHub.addPlaylist(pl);
